fddc5a70c337fd5024d90f85b0871a19b3bb2dad
Diego Sánchez <enano_sz@hotmail.com>
2022-06-06 20:33:51 +0200

    la api key está obsoleta

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  apiKey: string = 'da6b125e5424da695390356671ec27b8';
+  URI: string = '';
+
+  constructor(private http: HttpClient) {
+    this.URI = `https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&q=`;
+  }
+
+  getWeather(cityName: string, countryCode: string) {
+    return this.http.get(`${this.URI}${cityName},${countryCode}`);
+  }
+
+
+}
